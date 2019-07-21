5566650c93800aac98918f2eac37d97806ca1e52
Bryon <bryonlh@gmail.com>
2019-07-20 19:47:31 -0700

    addded some variables

--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var moment = require("moment");
+var axios = require("axios");
+var dotenv = require("DotEnv");
+var spotifyapi = require("Node-Spotify-API");
 
 var spotify = new Spotify(keys.spotify);
 
-console.log(spotify);
+// console.log(spotify);
