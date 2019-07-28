8938ea5d80fa1dc0a8cd23722b2ae0cddc4b304d
BryonLH <bryonlh@gmail.com>
2019-07-28 16:01:00 -0700

    Added Spotify function, more work needed

--- liri.js
+++ liri.js
@@ -4,8 +4,39 @@ var keys = require("./keys.js");
 var moment = require("moment");
 var axios = require("axios");
 var dotenv = require("DotEnv");
-var spotifyapi = require("Node-Spotify-API");
-
+var Spotify = require("Node-Spotify-API");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
+
 
 // console.log(spotify);
+
+var command = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+
+
+var spotifySong = function() {
+    spotify.search({ type: "track", query: term }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+        console.log(data);
+    });
+}
+
+// var movieSearch = function() {
+
+// }
+
+
+if (command === "spotify-this-song") {
+    // var songToSpotify = new 
+    spotifySong();
+    console.log("Command entered: " + command);
+    console.log("Song search: " + term);
+} else if (command === "movie-this") {
+    console.log("Command entered: " + command);
+    console.log("Movie search: " + term);
+} else if (command === "do-what-it-says") {
+    console.log("Command entered: " + command);
+}
